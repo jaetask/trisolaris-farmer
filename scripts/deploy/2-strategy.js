f1e5a78a72df77880db3bd40414c17b1803c6943
Magnus Brantheim <m.brantheim@gmail.com>
2022-04-08 10:34:44 +0200

    Upgrade strategy deploy script

--- scripts/deploy/2-strategy.js
+++ scripts/deploy/2-strategy.js
@@ -9,10 +9,18 @@ async function main() {
   const strategist1 = '0x1E71AEE6081f62053123140aacC7a06021D77348';
   const strategist2 = '0x81876677843D00a7D792E1617459aC2E93202576';
   const strategist3 = '0x1A20D7A31e5B3Bc5f02c8A146EF6f394502a10c4';
+  const wantAddress = '';
+  const poolId = 0;
 
   const strategy = await hre.upgrades.deployProxy(
     Strategy,
-    [vaultAddress, [treasuryAddress, paymentSplitterAddress], [strategist1, strategist2, strategist3]],
+    [
+      vaultAddress,
+      [treasuryAddress, paymentSplitterAddress],
+      [strategist1, strategist2, strategist3],
+      wantAddress,
+      poolId,
+    ],
     {kind: 'uups', timeout: 0},
   );
 
